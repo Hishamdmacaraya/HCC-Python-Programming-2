d10f1b8fe1f76d572aaca8cf1e82c479900a1471
Hisham D Macaraya <hisham.macaraya@gmail.com>
2024-07-20 16:20:16 -0500

    chore: Add SETMacarayaHisham.py file with introduction and greeting

---
+++ SETMacarayaHisham.py
@@ -0,0 +1,5 @@
+GREET = "Hello and welcome to our course!"
+P1 = '''My name is Hisham D Macaraya, my major is in Artificial Intelligence. I am currently on my second semester in my associate degree in AI. I am a full-time student and I am also working part-time as an AI Data Annotator working on labeling data for machine learning models of some of the top tech companies in the world. I am also a full-time father of a 4-year-old kid and we love playing at the park. I am excited to learn more about Python programming and how it can help me in my career.'''
+P2 = "Happy learning and coding everyone!"
+
+print(GREET + "\n" + P1 + "\n" + P2)
